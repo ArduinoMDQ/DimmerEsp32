4034ed212c06b69458f4fa38635f4df897a70143
Diego Giovanazzi <diegogiovanazzi@gmail.com>
2017-10-19 13:39:42 -0300

    Fix ACS712 FUNCIONAL
    led 6w I=30ma
    REsistiva 42W I=210ma
    fuincional.

--- DimmerEsp32.ino
+++ DimmerEsp32.ino
@@ -46,13 +46,15 @@ volatile int timing;
 /*
 Measuring AC Current Using ACS712
 */
-const int sensorIn = A0;
-double mVperAmp = 185.0;//185.0; // use 100 for 20A****66 for 30A **** 185 for 5A
+//const int sensorIn = A0;
+double mVperAmp = 0.185;//185.0; // use 100 for 20A****66 for 30A **** 185 for 5A
 double Voltage = 0;
 double VRMS = 0;
 double AmpsRMS = 0;
 int muestrasPromedio=0;
-
+const double vEsc = 3.3/4096.0 ;
+const float minUmbral =0.020;
+const float correccion =0.020;
 ////******** FIN ACS712////////
 
 /////////////******* LED TOUCH /////////
@@ -247,27 +249,20 @@ void task_ADC( void * parameter ){
   
   while(1){
     // Serial.println("valor medio " + String(analogRead(analogPin)));
-    delay(500) ;
-
-  /*  float valor = analogRead(analogPin)*(3.3 /4095.0);
-   Serial.println(" muestras" + String(analogRead(analogPin)));
-   Serial.println(" tension " + String(valor));
-  */
+    Serial.println("****************************************");
+    Serial.print("Valor Escalon 3.3 / 4096.0 : ");Serial.println(vEsc,8);
     float valor = TrueRMSMuestras();
-   
-    AmpsRMS=(valor*1000.0)/mVperAmp -.02;
-
-    
-    
-    if(AmpsRMS<0.015){   AmpsRMS=0;}  
-    
+    AmpsRMS=valor/mVperAmp - correccion;
+    Serial.print("AmpsRMS medido: "); Serial.println(AmpsRMS,3);
+    if(AmpsRMS < minUmbral){  AmpsRMS=0;}  
     String Potencia = String(220*AmpsRMS);
     String Corriente = String(AmpsRMS);
     Serial.print("AmpsRMS RMS: "); Serial.println(AmpsRMS,3);
     Serial.println("POWER RMS: " + String(Potencia));
     client.publish("casa/adc/potencia", (char*)Potencia.c_str());
     client.publish("casa/adc/corriente", (char*)Corriente.c_str());
-    
+    //vTaskDelay(3000/portTICK_PERIOD_MS);
+    delay(3000);
   }
 }
 
@@ -341,9 +336,9 @@ void setup() {
   ledcAttachPin(led_blue, LEDC_CHANNEL_1);
   ledcAttachPin(led_red, LEDC_CHANNEL_2);
 
-  xTaskCreate( task_ADC,"ADC",1000,NULL,1,NULL);
+  xTaskCreate( task_ADC,"ADC",10000,NULL,1,NULL);
   
-  touchAttachInterrupt(T8, gotTouch1, threshold);
+  //touchAttachInterrupt(T8, gotTouch1, threshold);
  
   digitalWrite(pin_controlDrimer,LOW);
   
@@ -487,43 +482,40 @@ void callback(char* topic, byte* payload, unsigned int length) {
       control_RGB=true;
       xTaskCreate( task_RGB_3,"RGB",1000,NULL,2,NULL);
       }
-   
-
-
-    
+     
   }
       
  }
 
 float TrueRMSMuestras(){
   
-  float result =0,conv=0,Acumulador=0,suma=0;
+  double result =0,conv=0,Acumulador=0,suma=0;
  int readValue =0;             //value read from the sensor
  int Count =0;
- float promedio =0;
+ double promedio =0;
  int sumatoria =0;
- const int n = 40;
- 
-
-  for(int i=0;i<n;i++){
+ const int n = 320;
+ int diferencia =0;
+ double Vo =0;
+ for(int i=0;i<n;i++){
+    //vTaskDelay(1/portTICK_PERIOD_MS);
     delay(1);
     sumatoria = sumatoria + analogRead(analogPin);
   } 
-  promedio=sumatoria/n;
-  Serial.println("Promedio "+ String(n)+" muestras:"+String(promedio));
-
+ promedio=sumatoria/n;
+ Vo=promedio*vEsc;
+ Serial.println("Tension Promedio : "+String(Vo));
  uint32_t start_time = millis();
-   
-  while((millis()-start_time )< 80){   
+ while((millis()-start_time )< 320){   
      Count++;
      readValue =  analogRead(analogPin);
-     conv=((readValue - promedio)*3.3)/4095.0;
+    
+     conv=(readValue - promedio)*vEsc;
      Acumulador=Acumulador+sq(conv);  
      }
    suma=Acumulador/Count;
    result=sqrt(suma);
-
-   Serial.println(readValue - promedio);
-   return result;
+   Serial.println(String(n)+" Muestras Promedio : "+String(promedio));
+  return result;
   }
 
